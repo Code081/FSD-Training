21f39ee89d28f3e9c2501494e8ea0eada362f663
Shashwat <shashwat@Shashwats-MacBook-Air.local>
2024-11-22 12:19:03 +0530

    completed assignment-1 day-8

--- Day-6,7,8/react-bundler/vite/vite-project/src/App.jsx
+++ Day-6,7,8/react-bundler/vite/vite-project/src/App.jsx
@@ -4,15 +4,16 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Card from './components/Card'
 import pic1 from "./images/logo-6.png"
+import State from './components/State'
 
 function App() {
   
 
   return (
     <div className='app'> 
-    <Card  name = "Shashwat Pandey" img = {pic1} roll = "57"/>
-    <Card  name = "Khushi" img = {pic1} roll = "35" />
-    <Card name = "Sarthak" img = {pic1} roll = "67" />
+    <div className='Counter'>
+      <State/>
+    </div>
     </div>
   )
 }
--- Day-6,7,8/react-bundler/vite/vite-project/src/components/State.jsx
+++ Day-6,7,8/react-bundler/vite/vite-project/src/components/State.jsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 
 const State = () => {
     const [count, setCount] = useState(0)
+    const[color, setColor] = useState('red')
 return (
     <div>
         <button onClick={()=>{
@@ -10,7 +11,18 @@ return (
         <button onClick={()=>{
             setCount(count-1)
         }}>Decreasing</button>
-        <h2>{Count}</h2>
+        <h2>{count}</h2>
+
+        <h1 style={{color : color }}>My Favourite Color is {color} !</h1>
+        <button onClick={()=>{
+            setColor('blue')
+        }}>Blue</button>
+        <button onClick={()=>{
+            setColor('red')
+        }}>Red</button>
+        <button onClick={()=>{
+            setColor('green')
+        }}>Green</button>
     </div>
 )
 }
